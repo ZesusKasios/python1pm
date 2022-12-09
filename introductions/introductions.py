d439a2b8c66972951bbd87fad8615043aff0bb97
ZesusKasios <ashimt004@gmail.com>
2022-12-09 11:18:14 +0545

    update

---
+++ introductions/introductions.py
@@ -0,0 +1,7 @@
+# name = "ashim"
+# email = "qwerty@gmail.com"
+# address = "kathmandu"
+# print(f"his name is {name} and his email is {email} and lastly his address is {address}")
+# x = int(input("enter the value of x:"))
+# y = int(input("enter the value of y:"))
+# print(x*y)
